e5749b913b46010e9444c9cd34d809a681bf033a
Richard S. Stewart <rstewart2702@gmail.com>
2016-10-14 11:25:20 -0500

    More unit tests to exercise btInsert.

--- TreeSetdotNet/Script.fsx
+++ TreeSetdotNet/Script.fsx
@@ -19,3 +19,52 @@ let myT =
    ( Tree(Datum("Stewart, Richard",1),Tree(Datum("Alvin",0),EmptyTree,EmptyTree),EmptyTree) ) ) =
   Tree(Datum("Alvin",1),EmptyTree,Tree(Datum("Stewart, Richard",0),EmptyTree,EmptyTree)) ;;
 
+btInsert EmptyTree "Smith"
+
+"Ockinga, Harold" |>
+btInsert (
+    "Lister, Hugh" |>
+    btInsert (
+      "James, Rick" |> 
+      btInsert (btInsert (btInsert (btInsert (btInsert myT "Smith, Bob") "Jones, Richard") "Hicks, Alfred") "Jackson, Gary")))
+
+[ "Smith, Bob"; "Jones, Richard"; "Hicks, Alfred"; "Jackson, Gary"; "James, Rick"; "Lister, Hugh"; "Ockenga, Harold";
+  "Orvis, Bob"; "Ostend, Dean"; "Jannson, Tove" ] |> List.fold btInsert EmptyTree
+(* 
+val it : string BalancedSearchTree =
+  Tree
+    (Datum ("Jones, Richard",3),
+     Tree
+       (Datum ("Jackson, Gary",2),
+        Tree (Datum ("Hicks, Alfred",0),EmptyTree,EmptyTree),
+        Tree
+          (Datum ("James, Rick",1),EmptyTree,
+           Tree (Datum ("Jannson, Tove",0),EmptyTree,EmptyTree))),
+     Tree
+       (Datum ("Ockenga, Harold",2),
+        Tree (Datum ("Lister, Hugh",0),EmptyTree,EmptyTree),
+        Tree
+          (Datum ("Ostend, Dean",1),
+           Tree (Datum ("Orvis, Bob",0),EmptyTree,EmptyTree),
+           Tree (Datum ("Smith, Bob",0),EmptyTree,EmptyTree))))
+*)
+
+// The following exposed a bug, probably with the rebalance function:
+
+[ "Smith, Bob"; "Jones, Richard"; "Hicks, Alfred"; "Jackson, Gary"; "James, Rick"; "Lister, Hugh"; "Ockenga, Harold" ]
+|> List.fold btInsert EmptyTree
+(* 
+val it : string BalancedSearchTree =
+  Tree
+    (Datum ("Jones, Richard",2),
+     Tree
+       (Datum ("Jackson, Gary",1),
+        Tree (Datum ("Hicks, Alfred",0),EmptyTree,EmptyTree),
+        Tree (Datum ("James, Rick",0),EmptyTree,EmptyTree)),
+     Tree
+       (Datum ("Ockenga, Harold",1),
+        Tree (Datum ("Lister, Hugh",0),EmptyTree,EmptyTree),
+        Tree (Datum ("Smith, Bob",0),EmptyTree,EmptyTree)))
+*)
+
+[ "Smith, Bob"; "Jones, Richard"; "Hicks, Alfred" ] |> List.fold btInsert EmptyTree
