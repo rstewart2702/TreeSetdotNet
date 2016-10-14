9087875e8b1142f29a27e02d73d4f79085cb2f2d
Richard S. Stewart <rstewart2702@gmail.com>
2016-10-14 15:05:01 -0500

    Much simplified btRemove function.  Eliminated match expressions, yet no compiler warnings.

--- TreeSetdotNet/SearchTrees.fs
+++ TreeSetdotNet/SearchTrees.fs
@@ -135,39 +135,75 @@ module BalancedBinaryTree =
         | Tree(Datum(k,_),EmptyTree,_) -> Some k
         | Tree(Datum(k,_),(Tree(_,_,_) as lc),_) -> findMin lc
 
-    let rec btRemove t k =
+    let rec btRemoveOld t k =
         match t with
         | EmptyTree -> EmptyTree
-        // The search key is in the root, and is the only value stored in the tree:
-        | Tree(Datum(k,0),EmptyTree,EmptyTree) -> EmptyTree
-        // There is no left child, and the search key is in the root of the tree:
-        | Tree(Datum(k,1),EmptyTree,rc) -> rc
-        // There is no right child, and the search key is in the root of the tree:
-        | Tree(Datum(k,1),lc,EmptyTree) -> lc
-        // The search key is NOT in the root:
+        // I had tried to use pattern matching inappropriately, in previous versions of
+        // this function.  Fundamentally, pattern-matching introduces new identifiers,
+        // new scope, and so if the free variables you introduce in a match expression
+        // have the same name as those in an enclosing scope, you lose access to those
+        // from the enclosing scope!  I had introduced patterns which mentioned a 
+        // variable k, under the mistaken assumption that the "k" used in the 
+        // pattern-matching clauses would be identical to the "k" in the parameter list
+        // of this btRemove function.  That's just not now it works in F#!
+        //
+        // So, each of the following patterns now contains an identifier called 
+        //   rootKey
+        // and we must explicitly compare rootKey to k in order to determine 
+        // whether or not the root of the tree happens to be the item named by k, above...
+        | Tree(Datum(rootKey,0),EmptyTree,EmptyTree) -> 
+            // If k is the item in the root, then the result is EmptyTree; otherwise, regurgitate the tree.
+            if rootKey = k then EmptyTree else t
+        // 
+        | Tree(Datum(rootKey,1),EmptyTree,rc) -> 
+            // If k is the item in the root, then the result is the right-hand child, rc
+            if rootKey = k then 
+                rc 
+            else
+            // If k is NOT the item in the root, 
+            // then the result is the tree derived from trying to remove the item from the right child
+            // and then reassembling the new version of the tree:
+                let newRc = (btRemoveOld rc k)
+                Tree(Datum(rootKey,(max -1 (tHeight newRc))+1),EmptyTree, newRc )
+        // 
+        | Tree(Datum(rootKey,1),lc,EmptyTree) -> 
+            // If k is the item in the root, then the result is the left-hand child, lc
+            if rootKey = k then 
+                lc 
+            else 
+            // If k is NOT the item in the root, 
+            // then the result is the tree derived from trying to remove the item from the left child
+            // and then reassembling the new version of the tree:
+                let newLc = (btRemoveOld lc k)
+                Tree(Datum(rootKey,(max (tHeight newLc) -1)+1), newLc, EmptyTree)
+        // The most general case, in which the tree must be taller than 0 or 1:
         | Tree(Datum(rootKey,h),lc,rc) -> 
             if k < rootKey then
                 // If the search key falls to the left of the root key:
                 // Calculate a new left tree by removing the item from the left-hand
                 // tree...
                 let newLc =
-                    btRemove lc k
+                    btRemoveOld lc k
                 // ...and calculate a new resulting tree by rebalancing the reassembled
                 // tree
-                rebalance (Tree(Datum(rootKey,(max (tHeight newLc) (tHeight rc))+1),
-                                newLc,
-                                rc))
+                rebalance (
+                    Tree(Datum(rootKey,(max (tHeight newLc) (tHeight rc))+1),
+                         newLc,
+                         rc)
+                )
             else if rootKey < k then
                 // If the search key falls to the right of the root key:
                 // Calculate a new right-hand tree by removing the searched-for item
                 // from the right-hand tree...
                 let newRc = 
-                    btRemove rc k
+                    btRemoveOld rc k
                 // ...and calculate a new resulting tree by rebalancing the reassembled
                 // tree:
-                rebalance (Tree(Datum(rootKey,(max (tHeight lc) (tHeight newRc))+1),
-                                lc,
-                                newRc))
+                rebalance (
+                    Tree(Datum(rootKey,(max (tHeight lc) (tHeight newRc))+1),
+                         lc,
+                         newRc)
+                )
             else 
                 // The rootKey = search key, k, so we must re-stitch the tree
                 // back together by deriving a new root from the minimum element
@@ -178,15 +214,76 @@ module BalancedBinaryTree =
                 match (findMin rc) with
                 | Some k ->
                     let newRc = 
-                        btRemove rc k
+                        btRemoveOld rc k
                     rebalance (
                         Tree(Datum(k, (max (tHeight lc) (tHeight newRc))+1),
-                                   lc,
-                                   newRc)
+                             lc,
+                             newRc)
                     )
                 | None -> EmptyTree
 
-            
+
+    // 
+    let rec btRemove t k =
+        // I had tried to use pattern matching inappropriately, in previous versions of
+        // this function.  Fundamentally, pattern-matching introduces new identifiers,
+        // new scope, and so if the free variables you introduce in a match expression
+        // have the same name as those in an enclosing scope, you lose access to those
+        // from the enclosing scope!  I had introduced patterns which mentioned a 
+        // variable k, under the mistaken assumption that the "k" used in the 
+        // pattern-matching clauses would be identical to the "k" in the parameter list
+        // of this btRemove function.  That's just not now it works in F#!
+        //
+        // So, each of the following patterns now contains an identifier called 
+        //   rootKey
+        // and we must explicitly compare rootKey to k in order to determine 
+        // whether or not the root of the tree happens to be the item named by k, above.
+        //
+        // It may indeed be significantly simpler to reason about the tree 
+        // in terms of the height field?
+        match t with
+        | EmptyTree -> EmptyTree
+        | Tree(Datum(rootKey,0),EmptyTree,EmptyTree) ->
+            // If the item appears in the root, and it is the only item in the tree,
+            // then the result is an EmptyTree.
+            // Otherwise, return the original tree, for the sought item, k,
+            // simply is not present.
+            // We thus take the philosophy that the result of trying to remove
+            // a non-existent item from a tree leaves the tree unchanged.
+            if rootKey = k then EmptyTree else t
+        | Tree(Datum(rootKey,_),lc,rc) ->
+            if k < rootKey then
+                let newLc = btRemove lc k
+                rebalance (
+                    Tree(Datum(rootKey,(max (tHeight newLc) (tHeight rc))+1),
+                         newLc,
+                         rc)
+                )
+            else if rootKey < k then
+                let newRc = btRemove rc k
+                rebalance (
+                    Tree(Datum(rootKey,(max (tHeight lc) (tHeight newRc))+1),
+                         lc,
+                         newRc)
+                )
+            else
+                match (findMin rc) with
+                | Some minKey ->
+                    let newRc = btRemove rc minKey
+                    rebalance (
+                        Tree(Datum(minKey,(max (tHeight lc) (tHeight newRc))+1),
+                             lc,
+                             newRc)
+                    )
+                | None ->
+                    // This means that the right-hand child rc was EmptyTree, 
+                    // which implies that the only option left to us is to
+                    // replace t with its left child.
+                    // Former versions of btRemove function prevented this
+                    // case from being evaluated with a longer, more complex
+                    // list of matching expressions.  This is much simpler.
+                    lc
+
     (* 
     Need several operations:
     + Create a treeset?
